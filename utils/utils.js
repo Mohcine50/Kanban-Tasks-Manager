4a5e4739bebb51e34184b865b957c41ef8cb0b64
Mohcine50 <mohcinela1998@gmail.com>
2022-10-11 22:52:10 +0100

    change bcrypt to bcryptjs

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const Joi = require("joi");
 exports.passwordCrypt = async (password) => {
   const salt = await bcrypt.genSalt(10);
